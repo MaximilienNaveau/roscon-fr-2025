fee4fb50f3078ed2be50ff57b4d782977db83dec
Maximilien Naveau <maximilien.naveau@pal-robotics.com>
2025-08-28 19:10:03 +0200

    Add a simple python node for launching the demo with/without an extra pal-statistics topic

--- README.md
+++ README.md
@@ -22,3 +22,17 @@ ros2 run  plotjuggler plotjuggler
 
 ### Phase 2 launch the custom node
 
+- Terminal 1
+```
+ros2 launch ros2_control_demo_example_1 rrbot.launch.py
+```
+
+- Terminal 2
+```
+ros2 launch pal_stats_demo test_forward_position_controller.launch.py
+```
+
+- Terminal 3
+```
+ros2 run  plotjuggler plotjuggler
+```
---
+++ pal_stats_demo/setup.py
@@ -0,0 +1,40 @@
+from setuptools import find_packages, setup
+
+package_name = 'pal_stats_demo'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        # ROS 2 package registration
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        # Install launch files
+        (
+            'share/' + package_name + '/launch',
+            ['launch/test_forward_position_controller.launch.py']
+        ),
+        # Install config files
+        (
+            'share/' + package_name + '/config',
+            ['config/rrbot_forward_position_publisher.yaml']
+        ),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Maximilien Naveau',
+    maintainer_email='maximilien.naveau@pal-robotics.com',
+    description='Demo package for pal_statistics introspection',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            (
+                'publisher_forward_position_controller = '
+                'pal_stats_demo.publisher_forward_position_controller:main'
+            ),
+        ],
+    },
+)
